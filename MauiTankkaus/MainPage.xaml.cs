38b928e8632f5abdae214fd9f2fe59b8558a871e
Stephanie Robertz <stephanie.robertz@gmail.com>
2023-10-30 10:46:56 +0200

    Add project files.

---
+++ MauiTankkaus/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiTankkaus;
+
+public partial class MainPage : TabbedPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void Nappi_Clicked(object sender, EventArgs e)
+	{
+
+	}
+}
+
